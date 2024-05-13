dcdca71dc089d2cfabfd9c9a131eed0939ccd912
Mrinmoy Deb <mrinmoydeb485@gmail.com>
2024-05-14 00:06:05 +0530

    minor changes

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ let list_el = document.createElement('ul');
 list_el.classList.add('list');
 let lists = document.createElement('li');
 list_el.appendChild(lists);
-let creatSpan = document.createElement('span');
-creatSpan.textContent = userInputs;
+let creatSpan = document.createElement('input');
+creatSpan.type = 'text';
+creatSpan.setAttribute('readonly', true);
+creatSpan.value = userInputs;
+creatSpan.classList.add('inputvalBox'); 
 lists.appendChild(creatSpan);
 let allActionBtns = document.createElement('span');
 allActionBtns.classList.add('btngrp');
@@ -20,6 +23,7 @@ lists.appendChild(allActionBtns);
 let checkBtn = document.createElement('i');
 checkBtn.classList.add('fa-solid');
 checkBtn.classList.add('fa-check');
+checkBtn.classList.add('editButton');
 let editBtn = document.createElement('i');
 editBtn.classList.add('fa-solid');
 editBtn.classList.add('fa-pen-to-square');
@@ -32,6 +36,14 @@ allActionBtns.appendChild(deletBtn);
 
 let listNode = document.querySelector(".list_node");
 listNode.appendChild(list_el)
+checkBtn.onclick = ()=>{
+    console.log('check');
+    if(checkBtn.classList.contains('editButton')){
+        creatSpan.classList.add('line')
+    }
+}
+
+
 }
 
 
